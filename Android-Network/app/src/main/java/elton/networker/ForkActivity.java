895db78f307db77021f2b6d564f26c9c863b1344
andreguzman <an.guzmn@gmail.com>
2015-09-26 18:57:59 -0400

    Added some functionality to ForkActivity
    It now opens some dummy activities on button click

--- Android-Network/app/src/main/java/elton/networker/ForkActivity.java
+++ Android-Network/app/src/main/java/elton/networker/ForkActivity.java
@@ -1,38 +1,48 @@
 package elton.networker;
 
 import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
-public class ForkActivity extends Activity {
+public class ForkActivity extends Activity implements View.OnClickListener{
+
+    private ImageButton addContactButton;
+    private ImageButton viewContactButton;
+    private ImageButton addTileButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fork);
+
+        addContactButton = (ImageButton) findViewById(R.id.addContactButton);
+        viewContactButton = (ImageButton) findViewById(R.id.viewContactButon);
+        addTileButton = (ImageButton) findViewById(R.id.addTileButton);
+
+        addContactButton.setOnClickListener(this);
+        viewContactButton.setOnClickListener(this);
+        addTileButton.setOnClickListener(this);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_fork, menu);
-        return true;
-    }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public void onClick(View view) {
+        if (view.getId() == R.id.addContactButton) {
+            Intent intent = new Intent(this, AddContactActivity.class);
+            this.startActivity(intent);
+        } else if (view.getId() == R.id.viewContactButon) {
+            Intent intent = new Intent(this, ViewContactActivity.class);
+            this.startActivity(intent);
+        } else if (view.getId() == R.id.addTileButton){
+            Intent intent = new Intent(this, MyTileActivity.class);
+            this.startActivity(intent);
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
